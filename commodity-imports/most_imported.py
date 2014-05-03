ec4cacbbbf44ed4147205d00fc24037d9644ed34
Suhas SG <jargnar@gmail.com>
2014-05-03 21:35:22 +0530

    added qty of import to most_imported

--- commodity-imports/most_imported.py
+++ commodity-imports/most_imported.py
@@ -5,14 +5,21 @@ DELIMITER = ","
 
 def most_imported():
     '''
-    Calculates top commodity imports by various metrics
+    Calculates top 5 commodity imports by various metrics
     '''
 
     #Read the data
     df = pd.read_csv(FILE, delimiter=DELIMITER)
+    cols = df.columns
 
-    #Top 5 commodities by number of countries imported from
-    print df[df.columns[0]].value_counts().head()
+    print "Top 5 commodities by number of countries imported from"
+    print df[cols[0]].value_counts().head()
+
+    print "Top 5 commodities by quantity of import during 2011-2012 and 2012-2013"
+    for col in cols[3], cols[5]:
+        print df.groupby(cols[0])[[col]].sum().sort([col], ascending=False).head()
+
+    print "Top 5 commodities by cost of import during 2011-2012 and 2012-2013"
 
 
 if __name__ == "__main__":
